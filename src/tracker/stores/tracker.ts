b6ec9ebc07a0d2471a08ae3c160330de4f5eb7ab
Jacob <spjak1@gmail.com>
2024-02-04 10:00:28 +0100

    Fix numbering of creatures with different display names

--- src/tracker/stores/tracker.ts
+++ src/tracker/stores/tracker.ts
@@ -295,7 +295,8 @@ function createTracker() {
             const creature = list[i];
             if (
                 creature.player ||
-                list.filter((c) => c.name == creature.name).length == 1
+                list.filter((c) => c.name == creature.name).length == 1 || 
+                list.filter((c) => c.display == creature.display).length == 1
             ) {
                 continue;
             }
